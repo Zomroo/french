9e2ca9e40c34a7b9b4602767c4aae080eaf8f856
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-16 23:51:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# API credentials
+API_ID = 16844842
+API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
+BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
+
+# MongoDB settings
+MONGODB_URI = 'mongodb+srv://Bot:Bot@cluster0.kse2h.mongodb.net/?retryWrites=true&w=majority'
+DB_NAME = "Rank"
+COLLECTION_NAME = "Up"
