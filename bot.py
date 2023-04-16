b4f1d05f8339bca9cdf5456232db7b6b11f34e85
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 02:20:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ import os
 API_ID = 16844842
 API_HASH = "f6b0ceec5535804be7a56ac71d08a5d4"
 BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
-MONGO_URI = 'mongodb+srv://Bot:Bot@cluster0.kse2h.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URL = 'mongodb+srv://Bot:Bot@cluster0.kse2h.mongodb.net/?retryWrites=true&w=majority'
 
 
 
