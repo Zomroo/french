7e5932dfab2cc47ae3228bc2418baa1bb339c97f
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 03:08:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pyrogram import Client
 import config
 from modules import rank
-from modules.rank import handle_me_command
+
 
 if __name__ == '__main__':
     try:
-        rank.app.add_handler(handle_me_command)
         rank.app.run()
 
     except ApiIdInvalid:
         print("Invalid API ID. Please set the correct API ID in the config.py file.")
     except ApiHashInvalid:
         print("Invalid API hash. Please set the correct API hash in the config.py file.")
+
+        
