f5279bcb596bbe98206805f7cc90c2a1a5d014e1
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 03:15:32 +0530

    Update alive.py

--- modules/alive.py
+++ modules/alive.py
@@ -1,7 +1,10 @@
+import config
 from pyrogram import Client, filters
+from pyrogram.types import Message
+from database.database import Database
 
-app = Client("my_bot")
-
+db = Database(config.MONGO_URI, config.MONGO_DB_NAME)
+app = Client("my_bot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
 
 @app.on_message(filters.command("alive", prefixes="/") & filters.group)
 def alive_command_handler(client, message):
