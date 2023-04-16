0b0a9080a988f2647b4c363956e628e13f0649a0
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 00:05:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ BOT_TOKEN = "5931504207:AAF-jzKC8USclrFYrtcaeAZifQcmEcwFNe4"
 
 # MongoDB settings
 MONGO_URI = 'mongodb+srv://Bot:Bot@cluster0.kse2h.mongodb.net/?retryWrites=true&w=majority'
-DB_NAME = "Rank"
+MONGO_DB_NAME = "Rank"
 COLLECTION_NAME = "Up"
