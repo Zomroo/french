6fb4e260b4a4bc195ab00462b2ccf5b3848bed34
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 03:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from pyrogram import Client
 import config
 from modules import rank
 
-
 if __name__ == '__main__':
-    try:
-        rank.app.run()
+    app = Client("my_bot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
 
+    # Add message handlers from the rank module
+    app.add_handler(rank.handle_message)
+    app.add_handler(rank.handle_me_command)
+
+    try:
+        app.run()
     except ApiIdInvalid:
         print("Invalid API ID. Please set the correct API ID in the config.py file.")
     except ApiHashInvalid:
         print("Invalid API hash. Please set the correct API hash in the config.py file.")
-
-        
