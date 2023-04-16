05bd302905eb373b6f4be4d146a3b97a0a7080f4
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-16 23:42:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from pyrogram.errors import ApiIdInvalid, ApiHashInvalid
+import config
+from modules import rank
+
+
+if __name__ == '__main__':
+    try:
+        rank.app.run()
+    except ApiIdInvalid:
+        print("Invalid API ID. Please set the correct API ID in the config.py file.")
+    except ApiHashInvalid:
+        print("Invalid API hash. Please set the correct API hash in the config.py file.")
