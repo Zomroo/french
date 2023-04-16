9f7259c2d3d2a602b81c3578fce28154098559a5
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 01:53:46 +0530

    Update rank.py

--- modules/rank.py
+++ modules/rank.py
@@ -37,7 +37,7 @@ def handle_message(client: Client, message: Message):
 
         
 # Define command handler
-@app.on_message(filters.command("me"))
+@app.on_message(filters.command("me") & (filters.private | filters.group))
 def handle_me_command(client: Client, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
@@ -50,7 +50,7 @@ def handle_me_command(client: Client, message: Message):
         level = user_data["level"]
         rank_name = get_rank_name(level)
         points_to_next_rank = get_points_to_next_rank(level, points)
-        client.send_message(chat_id, f"Your current rank is {rank_name} ({points} points). {points_to_next_rank} points to next rank.")
+        client.send_message(chat_id, f"Your current rank in this group is {rank_name} ({points} points). {points_to_next_rank} points to next rank.")
 
 def get_level(points: int) -> int:
     for i, rank in enumerate(RANKS):
