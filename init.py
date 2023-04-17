746495f95972a28d9ed3a2931abf6c8d5b7bf378
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 20:15:11 +0530

    Update init.py

--- init.py
+++ init.py
@@ -5,7 +5,5 @@ app = Client(
     session_name=config.SESSION_NAME,
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-    workers=config.WORKERS,
-    workdir=config.WORKDIR
+    bot_token=config.BOT_TOKEN
 )
