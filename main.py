057cd97afb780df54ee31e4bd1e56d6cfae36d20
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-04-17 20:17:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import config
 from modules import alive
 from modules import rank
 
-
 if __name__ == '__main__':
     try:
-        rank.app.run()
-
-    except ApiIdInvalid:
-        print("Invalid API ID. Please set the correct API ID in the config.py file.")
-    except ApiHashInvalid:
-        print("Invalid API hash. Please set the correct API hash in the config.py file.")
-
-        
+        alive.app.start()
+        rank.app.start()
+    except KeyboardInterrupt:
+        print('\nExiting...')
+    finally:
+        alive.app.stop()
+        rank.app.stop()
